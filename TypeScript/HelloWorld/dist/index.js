34060723aa60bc585ea91b6edd9d0a77fc604ef5
Samson Kinyanjui <sammainah98@gmail.com>
2025-06-04 15:39:17 +0300

    removed

--- TypeScript/HelloWorld/dist/index.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-console.log("Hello World!!!!!!!!!!!!!!!!1");
-let name1 = "Samson";
-let age = 27;
-console.log("My Name is :", name1, ", and", "My Age is :", age);
-//# sourceMappingURL=index.js.map
